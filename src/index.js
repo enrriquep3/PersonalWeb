5394f8d52b6ff4aaa937ea582ffff4149021e1fa
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-12-22 20:03:57 -0500

    deploy9

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-D6TFQQJTWK'); 
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
