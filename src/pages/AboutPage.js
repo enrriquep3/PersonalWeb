685354a8ebc0604b371cb732e94ddc9a7e7f4f95
enrrique pimentel <enrriquep3@gmail.com>
2021-04-06 11:02:15 -0400

    last-changes

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
 import "./styles.css";
-import RadarChart from '../assets/images/radar.jpg'
+import RadarChart from '../assets/images/radar.png'
 
 import Pdf from '../assets/images/resume.pdf';
 
@@ -13,7 +13,7 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <p>Hi, my name is Enrrique Pimentel. I'm a creative, problem solver and hard working Web Developer with experience in  JavaScript,  Node Js, HTML5, React,  MongoDB, MySQL.</p>
+            <p>Hi, my name is Enrrique Pimentel. I'm a creative, problem solver and hard working Web Developer & Designer with experience in  JavaScript,  Node Js, HTML5, WordPress, Elementor  MongoDB, MySQL.</p>
 
 
             <p>I'm constantly learning new technologies and improving myself. Currently those things include gaining more experience with ES6, React, Hooks, Redux, and Node.</p>
@@ -22,7 +22,7 @@ function AboutPage(props) {
 
             <p>Check out my <a href = {Pdf} target = "_blank" rel="noopener noreferrer">resume</a>.</p>
 
-            <p>I am passionate to build, create and develop new ideas into products and services. Let's Create Great Things! </p>
+            <p>I am passionate to build, create and develop new ideas into products and services. Let's Create Amazing Things! </p>
 
             </Content>
             <img className="radarChart" src={RadarChart} alt="RadarChart"/>
